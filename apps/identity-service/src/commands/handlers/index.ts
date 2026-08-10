53fc219f3479073de12a904bc55ac5ed67654d26
sincere-abayo <abayosincere11@gmail.com>
2026-08-10 06:20:28 +0200

    Remove duplicate CreateTenant from identity-service
    - Deleted create-tenant.handler.ts from identity-service
    - Deleted create-tenant.command.ts from identity-service
    - Updated handlers index.ts to remove CreateTenantHandler export
    - Tenant creation is now handled exclusively by tenant-service

--- apps/identity-service/src/commands/handlers/index.ts
+++ apps/identity-service/src/commands/handlers/index.ts
@@ -1,5 +1,4 @@
-import { CreateTenantHandler } from './create-tenant.handler.js';
 import { LoginUserHandler } from './login-user.handler.js';
 import { CreateUserHandler } from './create-user.handler.js';
 
-export const CommandHandlers = [CreateTenantHandler, LoginUserHandler, CreateUserHandler];
+export const CommandHandlers = [LoginUserHandler, CreateUserHandler];
